ce8deb7723d79add0fe1ecc190fdddc71eb85fb1
Kevinstar-09 <kevin.vides1@catolica.edu.sv>
2025-02-27 18:02:04 -0600

    Actualizacion

---
+++ P01_2022VF650_2022MV652/Controladores/espaciosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P01_2022VF650_2022MV652.Controladores
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class espaciosController : ControllerBase
+    {
+    }
+}
---
+++ P01_2022VF650_2022MV652/Models/espacios.cs
@@ -0,0 +1,6 @@
+﻿namespace P01_2022VF650_2022MV652.Models
+{
+    public class espacios
+    {
+    }
+}
